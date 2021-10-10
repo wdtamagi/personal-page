3eb34d8b2172aa010333fc339af1caa56187d4b6
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-10-10 19:00:04 -0300

    Add header component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+  import NavItem from "./NavItem.svelte";
+
+  const itemList = [
+    { text: "home", link: "/" },
+    { text: "blog", link: "/blog" },
+    { text: "resume", link: "/resume" },
+  ];
+</script>
+
+<header>
+  <nav class="container mx-auto py-8">
+    <ul class="sm:flex justify-between text-xl uppercase">
+      {#each itemList as item}
+        <NavItem {...item} />
+      {/each}
+    </ul>
+  </nav>
+</header>
---
+++ src/components/Header/NavItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let link: string;
+  export let text: string;
+</script>
+
+<li>
+  <a href={link} class="hover:text-purple-900 font-bold">
+    {text}
+  </a>
+</li>
